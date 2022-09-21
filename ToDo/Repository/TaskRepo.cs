2ccd0be4609177a0cf69ad3b0322693da7f2c14c
1micr01 <elias11a.e@gmail.com>
2022-09-21 09:01:36 +0200

    summaries

--- ToDo/Repository/TaskRepo.cs
+++ ToDo/Repository/TaskRepo.cs
@@ -11,12 +11,29 @@ namespace ToDo.Repository
             _toDoTasks = new List<ToDoTask>();
         }
 
+        /// <summary>
+        /// Adds a task
+        /// </summary>
+        /// <param name="task"></param>
         public void AddTask(ToDoTask task) => _toDoTasks.Add(task);
 
+        /// <summary>
+        /// Gets a task by a guid
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns>The task if there is only one</returns>
         public ToDoTask? GetTask(Guid guid) => _toDoTasks.SingleOrDefault(task => task.GUID == guid);
 
+        /// <summary>
+        /// Gets the whole list of tasks
+        /// </summary>
+        /// <returns>Returns the list of tasks</returns>
         public List<ToDoTask> GetAllTasks() => _toDoTasks.OrderBy(task => task.Created).ToList();
 
+        /// <summary>
+        /// Updates the task with the given values
+        /// </summary>
+        /// <param name="toDoTask"></param>
         public void EditTask(ToDoTask toDoTask)
         {
             ToDoTask? task = GetTask(toDoTask.GUID);
@@ -26,11 +43,21 @@ namespace ToDo.Repository
             task.IsCompleted = toDoTask.IsCompleted;
         }
 
+        /// <summary>
+        /// Removes the task by guid
+        /// </summary>
+        /// <param name="guid"></param>
         public void DeleteTask(Guid guid) => _toDoTasks.Remove(GetTask(guid));
 
+        /// <summary>
+        /// Removes all completed tasks
+        /// </summary>
         public void DeleteCompletedTasks() => _toDoTasks.RemoveAll(task => task.IsCompleted);
 
+        /// <summary>
+        /// Complete a task by guid
+        /// </summary>
+        /// <param name="guid"></param>
         public void CompleteTask(Guid guid) => GetTask(guid).IsCompleted = true;
-
     }
 }
